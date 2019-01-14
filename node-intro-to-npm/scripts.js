f13eb3a16671bb004598aaeed81270e6b31fe920
Jim Owens <jimcowens@gmail.com>
2019-01-14 11:18:34 -0500

    made package

---
+++ node-intro-to-npm/scripts.js
@@ -0,0 +1,20 @@
+console.log('sanity check')
+// this won't work anymore, because there isn't a windo, or a body!
+// document.querySelector('body')
+
+let atlTeams = [
+    'Braves',
+    'Falcons',
+    'Hawks',
+    'United',
+]
+// altTeams = [
+//     baseball :'Braves',
+//     football :'Falcons',
+//     bball :'Hawks',
+//     fütbol :'United',
+// ]
+const os = require ('os')
+console.log(os)
+// console.log(os.cpus())
+console.log(`freemem is `+ os.freemem())
