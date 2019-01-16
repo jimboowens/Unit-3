dcf9acd77d959c69abb40d4f73055be21c707ac5
Jim Owens <jimcowens@gmail.com>
2019-01-16 11:32:55 -0500

    get it

--- Callback-day-3/index.js
+++ Callback-day-3/index.js
@@ -1,2 +1,9 @@
 const express = require('express')
-console.log(express)
+// console.log(express)
+console.log('this is working')
+const app = express()
+
+app.get('/', (req, res)=>{
+    res.send('<h1>Hello, World!</h1>')
+})
+app.listen(3000)
