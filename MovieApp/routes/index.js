b868a06792f30216c3a24ab29508acd253fa4571
Jim Owens <jimcowens@gmail.com>
2019-01-17 14:52:19 -0500

    get it

--- MovieApp/routes/index.js
+++ MovieApp/routes/index.js
@@ -56,4 +56,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/search-movies/movie', (req,res)=>{
+res.json(req.body)
+})
+
 module.exports = router;
