f251ab88b7ccb6f5be4a049c7cd7d4e097fbf298
Jim Owens <jimcowens@gmail.com>
2019-01-23 09:36:06 -0500

    added

--- express_async_frame/index.js/routes/index.js
+++ express_async_frame/index.js/routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var request = require ('request')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+router.get('/', function(req,res, next){
+  function getNowPlaying(){
+    return new Promise((resolve,reject)=>{
+        request.get(nowPlayingUrl,(err,response,body)=>{
+            resolve(JSON.parse(body))
+        })
+    })
+}
+
+function getCast(movieId){
+    return new Promise((resolve, reject)=>{
+        const castUrl = `${apiBaseUrl}/movie/${movieId}/credits?api_key=${apiKey}`
+        request.get(castUrl,(err,response,body)=>{
+        resolve(JSON.parse(body))
+        })
+    })
+}
+
+function getPerson(personId){
+    return new Promise((resolve,reject)=>{
+        const personUrl = `${apiBaseUrl}/person/${personId}/?api_key=${apiKey}`
+        request.get(personUrl,(err,response,body)=>{
+            resolve(JSON.parse(body))
+        })
+    })
+}
+
+// async before a function means an await is coming
+
+async function run(){
+    const movieData = await getNowPlaying()
+    const castData = await getCast(movieData.results[0].id)
+    const personData = await getPerson(castData.cast[0].id)
+    console.log(movieData,castData,personData)
+}
+run()
+})
 
 module.exports = router;
